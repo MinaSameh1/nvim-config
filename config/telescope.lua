6f36150500236f56b834fa3c58625fd41f05021d
MinaSameh1 <minasameh1@gmail.com>
2022-01-28 17:09:50 +0200

    Added keybinds and changed settings

--- config/telescope.lua
+++ config/telescope.lua
@@ -3,10 +3,51 @@ local actions = require'telescope.actions'
 
 require'telescope'.setup {
   defaults = {
+    vimgrep_arguments ={
+      "rg",
+      "--color=never",
+      "--no-heading",
+      "--with-filename",
+      "--line-number",
+      "--column",
+      "--smart-case",
+    },
     prompt_prefix = " ",
     selection_caret = " ",
     path_display = { "smart" }
     },
+      initial_mode = "insert",
+      selection_strategy = "reset",
+      sorting_strategy = "ascending",
+      layout_strategy = "horizontal",
+      layout_config = {
+         horizontal = {
+            prompt_position = "top",
+            preview_width = 0.55,
+            results_width = 0.8,
+         },
+         vertical = {
+            mirror = false,
+         },
+         width = 0.87,
+         height = 0.80,
+         preview_cutoff = 120,
+      },
+      file_sorter = require("telescope.sorters").get_fuzzy_file,
+      file_ignore_patterns = { "node_modules" },
+      generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
+      path_display = { "truncate" },
+      winblend = 0,
+      border = {},
+      borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+      color_devicons = true,
+      use_less = true,
+      set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
+      file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+      grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+      qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+      -- Developer configurations: Not meant for general override
+      buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
     mappings = {
         i = {
             ["<C-n>"] = actions.cycle_history_next,
@@ -97,9 +138,8 @@ local Opts = { noremap = true, silent = true }
 Key( 'n', '<leader>ff', '<Cmd>Telescope find_files<CR>', Opts )
 Key( 'n', '<leader>fg', '<Cmd>Telescope live_grep<CR>', Opts )
 Key( 'n', '<leader>fb', '<Cmd>Telescope buffers<CR>' , Opts  )
-Key( 'n', '<leader>fB', '<Cmd>Telescope marks<CR>', Opts )
+Key( 'n', '<leader>fm', '<Cmd>Telescope marks<CR>', Opts )
 Key( 'n', '<leader>fH', '<Cmd>Telescope help_tags<CR>' , Opts)
-Key( 'n', '<leader>fa', '<Cmd>Telescope live_grep<CR>' , Opts)
 Key( 'n', '<leader>fh', '<Cmd>Telescope oldfiles<CR>' , Opts)
 Key( 'n', '<leader>fsh', '<Cmd>Telescope search_history<CR>' , Opts)
 Key( 'n', '<leader>fc', '<Cmd>Telescope commands<CR>' , Opts)
