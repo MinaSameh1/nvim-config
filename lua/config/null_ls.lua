975bf5ae79b0e6afb8fd2782cb1637267bc59568
MinaSameh1 <minasameh1@gmail.com>
2022-02-06 11:18:03 +0200

    Configed Null_ls

--- lua/config/null_ls.lua
+++ lua/config/null_ls.lua
@@ -1,14 +1,50 @@
 local null_ls = require("null-ls")
 
 null_ls.setup({
-  sources = {
-    null_ls.builtins.formatting.stylua,
-    null_ls.builtins.diagnostics.eslint,
-    null_ls.builtins.completion.spell,
-    null_ls.builtins.formatting.prettier.with({
-      filetypes = { "html", "json", "yaml", "markdown" },
-    }),
-    null_ls.builtins.diagnostics.write_good,
-    -- null_ls.builtins.code_actions.gitsigns,
-  },
+	sources = {
+
+    ---- Lua
+		null_ls.builtins.formatting.stylua,
+    null_ls.builtins.diagnostics.selene,
+    -- null_ls.builtins.diagnostics.luacheck, -- Slow
+
+    ---- Python
+		null_ls.builtins.formatting.black,
+		-- null_ls.builtins.formatting.autopep8, Gonna try black
+    null_ls.builtins.diagnostics.pydocstyle,
+		null_ls.builtins.diagnostics.pylint.with({
+			method = null_ls.methods.DIAGNOSTICS_ON_SAVE,
+		}),
+
+    ---- C / Cpp
+		null_ls.builtins.formatting.clang_format,
+		null_ls.builtins.diagnostics.cppcheck,
+
+    ---- Typescript/Javascript
+		null_ls.builtins.formatting.prettier.with({
+			env = {
+				PRETTIERD_DEFAULT_CONFIG = vim.fn.expand("~/.config/nvim/utils/linter-config/.prettier.config.js"),
+			},
+			prefer_local = "node_modules/.bin",
+		}),
+    null_ls.builtins.diagnostics.tsc,
+		null_ls.builtins.diagnostics.eslint_d,
+    null_ls.builtins.code_actions.eslint_d,
+
+    ---- Bash and Fish
+		null_ls.builtins.formatting.fish_indent,
+		null_ls.builtins.diagnostics.shellcheck.with({
+			-- diagnostics_format = "[#{c}] #{m} (#{s})",
+		}),
+
+    ---- General
+		null_ls.builtins.diagnostics.write_good.with({
+			filetypes = { "markdown", "latex" },
+		}),
+    null_ls.builtins.diagnostics.yamllint,
+		-- null_ls.builtins.diagnostics.php,
+
+		null_ls.builtins.completion.spell,
+		-- null_ls.builtins.code_actions.gitsigns,
+	},
 })
