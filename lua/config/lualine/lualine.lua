d01bbb1aca1ba7f7967f26b93ea78e7f9784a7d7
MinaSameh1 <minasameh1@gmail.com>
2022-03-17 22:31:30 +0200

    added LSP_Name to utils instead.

--- lua/config/lualine/lualine.lua
+++ lua/config/lualine/lualine.lua
@@ -1,24 +1,3 @@
-LSP_Name = {
-  -- Lsp server name .
-  function()
-    local msg = 'No Active Lsp'
-    local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
-    local clients = vim.lsp.get_active_clients()
-    if next(clients) == nil then
-      return msg
-    end
-    for _, client in ipairs(clients) do
-      local filetypes = client.config.filetypes
-      if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-        return client.name
-      end
-    end
-    return msg
-  end,
-  icon = ' LSP:',
-  color = { fg = '#ffffff', gui = 'bold' },
-}
-
 require('lualine').setup({
   options = {
     icons_enabled = true,
@@ -66,7 +45,7 @@ require('lualine').setup({
         end,
       },
       '%=',
-      LSP_Name,
+      require('config.utils').LSP_Name,
     },
     lualine_x = { 'encoding', 'fileformat', 'filetype' },
     lualine_y = { 'progress' },
