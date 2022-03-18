90c46ed97ae4b7c3008fde9c40f8c05b0b1d2108
MinaSameh1 <minasameh1@gmail.com>
2022-03-18 02:10:44 +0200

    misc: added comments.

--- lua/config/dap/dap_workspace_config.lua
+++ lua/config/dap/dap_workspace_config.lua
@@ -1,9 +1,12 @@
+-- Main idea came from https://github.com/mfussenegger/nvim-dap/discussions/337
+-- Pls notice if you want you could check out .exrc instead of this mess
 local dap_workspace_config = {}
 
 local config_folder = '.nvim'
 local config_file = '.nvim/launch.lua'
 local configurations = require('dap').configurations
 
+--- Loads the config file if found and notifies you.
 function dap_workspace_config.loadLunchLua()
   local config = loadfile(config_file, 't')
   if config == nil then
@@ -14,55 +17,69 @@ function dap_workspace_config.loadLunchLua()
   config()
 end
 
-function dap_workspace_config.insert(dap_lang_tables, config_table)
-  if dap_lang_tables == nil then
-    print('No config found for current debug, add a defualt config.')
+--- Responsible for adding configuration from file to dap configs.
+-- Loops ever entries allowing to have multiple configs in file.
+-- if config already exists override don't create a new entry.
+-- @param dap_lang_conf The dap configuration for language
+-- @param config_table A table of dap configuration tables to be appeneded to dap config for language.
+function dap_workspace_config.insert(lang, config_table)
+  local dap_lang_conf
+  if type(lang) == 'string' then -- If lang is passed as string then get its config.
+    dap_lang_conf = configurations[lang]
+  else
+    dap_lang_conf = lang
+  end
+  if dap_lang_conf == nil then -- if no default config found bailout.
+    print(
+      'No config found for current filetype, add a defualt config to dap first.'
+    )
     return
   end
   for _, tbl in ipairs(config_table) do
-    for index, tbl_dap in ipairs(dap_lang_tables) do
+    for index, tbl_dap in ipairs(dap_lang_conf) do
       if tbl_dap.name == tbl.name then
-        table.remove(dap_lang_tables, index)
+        -- TODO: Instead of deleteing the old entry instead change the values and add new keys accordingly.
+        table.remove(dap_lang_conf, index) -- Remove old entry
         break
       end
     end
-    table.insert(dap_lang_tables, tbl)
+    table.insert(dap_lang_conf, tbl) -- Add entry
   end
 end
 
+--- Gets the default config for filetype.
+-- @param filetype The filetype that will be used.
 local function get_config_filetype(filetype)
-  local default = {
-    'local dap = require("dap")',
+  local default = { -- Required and default
     'local insert = require("config.dap.dap_workspace_config").insert',
     '',
-    'insert(dap.configurations.' .. filetype .. ', {',
+    'insert("' .. filetype .. '", {',
     '	{',
   }
   local local_table = default
-  local local_config = configurations[filetype][1]
+  local local_config = vim.tbl_deep_extend( -- Get the main config.
+    'keep',
+    vim.empty_dict(),
+    configurations[filetype][1]
+  )
+  local_config.name = 'Debug (Custom Project Config)'
   for k, v in pairs(local_config) do
     if type(v) == 'string' then
-      if k == 'name' then
-        table.insert(
-          local_table,
-          '		' .. k .. ' = "Debug (Custom Project Config)",'
-        )
-      else
-        table.insert(local_table, '		' .. k .. ' = "' .. tostring(v) .. '",')
-      end
+      table.insert(local_table, '		' .. k .. ' = "' .. tostring(v) .. '",')
     else
       table.insert(local_table, '		' .. k .. ' = ' .. tostring(v) .. ',')
     end
   end
-  table.insert(local_table, '	},')
-  table.insert(local_table, '})')
+  table.insert(local_table, '	},') -- add a new line with bracket
+  table.insert(local_table, '})') -- close the first bracket
   return local_table
 end
 
+--- Main function responsible for editing config or creating a new one
 function dap_workspace_config.config()
-  if loadfile(config_file, 't') then
+  if loadfile(config_file, 't') then -- if file exists
     vim.cmd('edit ' .. config_file)
-    return
+    return -- load it and exit func
   end
   vim.loop.fs_mkdir(config_folder, 493) -- Create the folder
   local file_ok, fd = pcall(vim.loop.fs_open, config_file, 'w', 420) -- Then the file is created.
@@ -75,11 +92,15 @@ function dap_workspace_config.config()
   local filetype = vim.bo.filetype -- Save file type before switching buffers
   vim.cmd('edit ' .. config_file) -- Open buffer
   if configurations[filetype] == nil then
+		-- at this stage assume the user knows what they are doing,
+		-- the file will still get sourced and
+		-- to prevent normal users from ruining their dap config bailout.
     print('Warnning No default config found! Bailing out')
   else
     vim.api.nvim_buf_set_lines(0, 0, -1, 0, get_config_filetype(filetype))
     vim.api.nvim_command('write') -- Save it.
   end
+	--- TODO: Fix this ->
   -- local au_group = vim.api.nvim_create_augroup(
   --   'local_dap_config',
   --   { clear = true }
@@ -91,13 +112,14 @@ function dap_workspace_config.config()
   --   desc = 'Auto reloads launch.lua file on bufwrite.',
   -- })
   vim.cmd([[
-  " This is set till I use nvim new autocmds api, which is not yet on arch as of the time writing this.
-  augroup local_dap_config
+	" This is set till I use nvim new autocmds api, which I am too lazy to learn.
+	augroup local_dap_config
 		autocmd BufWritePost .nvim/launch.lua source .nvim/launch.lua
-  augroup END
-  ]])
+	augroup END
+	]])
 end
 
+--- Check for config file and loads it, also creates command for creation of said file or edit
 function dap_workspace_config.init()
   dap_workspace_config.loadLunchLua()
   vim.cmd(
