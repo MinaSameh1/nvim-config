deccd93d3945c283cf275c06802aa8b874425b60
Mina Sameh <mina.s.wadie@gmail.com>
2023-02-13 10:12:47 +0200

    Misc updated lua server

--- lua/config/lsp/NeovimLSP.lua
+++ lua/config/lsp/NeovimLSP.lua
@@ -149,18 +149,22 @@ mason_lspconfig.setup_handlers({
     local opts = default_opts
     opts.settings = {
       Lua = {
+        runtime = {
+          -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+          version = 'LuaJIT',
+        },
         diagnostics = {
           -- Get the language server to recognize the `vim` global
           globals = { 'vim' },
         },
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file('', true),
-      },
-      -- Do not send telemetry data containing a randomized but unique identifier
-      telemetry = {
-        enable = false,
+        workspace = {
+          -- Make the server aware of Neovim runtime files
+          library = vim.api.nvim_get_runtime_file('', true),
+        },
+        -- Do not send telemetry data containing a randomized but unique identifier
+        telemetry = {
+          enable = false,
+        },
       },
       format = false, -- use stylua
     }
