8cc6f918154c55b339d53cf28e6e37954770d3a2
MinaSameh1 <minasameh1@gmail.com>
2023-02-25 14:59:32 +0200

    Add System open to neotree

--- lua/config/neotree.lua
+++ lua/config/neotree.lua
@@ -127,6 +127,7 @@ neotree.setup({
       ['?'] = 'show_help',
       ['<'] = 'prev_source',
       ['>'] = 'next_source',
+      ['O'] = 'system_open',
     },
   },
   nesting_rules = {},
@@ -177,6 +178,17 @@ neotree.setup({
         ['[g'] = 'prev_git_modified',
         [']g'] = 'next_git_modified',
       },
+    },
+    commands = {
+      system_open = function(state)
+        local node = state.tree:get_node()
+        local path = node:get_id()
+        -- macOs: open file in default application in the background.
+        -- Probably you need to adapt the Linux recipe for manage path with spaces. I don't have a mac to try.
+        vim.api.nvim_command('silent !open -g ' .. path)
+        -- Linux: open file in default application
+        vim.api.nvim_command(string.format("silent !xdg-open '%s'", path))
+      end,
     },
   },
   buffers = {
