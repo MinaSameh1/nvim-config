ff6002763407cb7fb3ac75ce2b61ecaf856ccc68
MinaSameh1 <minasameh1@gmail.com>
2023-02-25 14:59:32 +0200

    Add System open to neotree

--- lua/config/neotree.lua
+++ lua/config/neotree.lua
@@ -183,13 +183,6 @@ neotree.setup({
       system_open = function(state)
         local node = state.tree:get_node()
         local path = node:get_id()
-<<<<<<< HEAD
-        -- macOs: open file in default application in the background.
-        -- Probably you need to adapt the Linux recipe for manage path with spaces. I don't have a mac to try.
-        vim.api.nvim_command('silent !open -g ' .. path)
-        -- Linux: open file in default application
-=======
->>>>>>> 6bd0c4d (Add System open to neotree)
         vim.api.nvim_command(string.format("silent !xdg-open '%s'", path))
       end,
     },
