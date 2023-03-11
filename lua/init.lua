65c97d88eae9e7f7d5f348c338c73fdfba507dee
MinaSameh1 <minasameh1@gmail.com>
2023-03-11 11:22:29 +0200

    Add basic support for semntaic highlighting for now

--- lua/init.lua
+++ lua/init.lua
@@ -3,7 +3,6 @@
 -- *         Plugins        *
 -- **************************
 --]]
-
 local utils = require('config.utils')
 local xmap = utils.xmap
 local nnoremap = utils.nnoremap
@@ -37,5 +36,50 @@ require('config.plugins')
 require('config.colorscheme')
 require('colors.colors')
 require('cmds')
+
+-- If your colorscheme doesn't define @lsp.* groups yet,
+-- but it does define treesitter highlights,
+-- you might find it useful to link the semantic groups to the treesitter groups
+-- to get consistent colors:
+local links = {
+  ['@lsp.type.namespace'] = '@namespace',
+  ['@lsp.type.type'] = '@type',
+  ['@lsp.type.class'] = '@type',
+  ['@lsp.type.enum'] = '@type',
+  ['@lsp.type.interface'] = '@type',
+  ['@lsp.type.struct'] = '@structure',
+  ['@lsp.type.parameter'] = '@parameter',
+  ['@lsp.type.variable'] = '@variable',
+  ['@lsp.type.property'] = '@property',
+  ['@lsp.type.enumMember'] = '@constant',
+  ['@lsp.type.function'] = '@function',
+  ['@lsp.type.method'] = '@method',
+  ['@lsp.type.macro'] = '@macro',
+  ['@lsp.type.decorator'] = '@function',
+}
+
+for newgroup, oldgroup in pairs(links) do
+  vim.api.nvim_set_hl(0, newgroup, { link = oldgroup, default = true })
+end
+
+vim.api.nvim_create_autocmd('ColorScheme', {
+  -- Colors: Purple
+  callback = function()
+    vim.api.nvim_set_hl(0, '@lsp.mod.readonly', { italic = true })
+    vim.cmd([[
+    " hi @lsp.type.class      guifg=Aqua
+    " hi @lsp.type.function   guifg=Yellow
+    hi @lsp.type.method     guifg=Orange
+    hi @lsp.type.parameter  guifg=Purple
+    hi @lsp.type.variable   guifg=Yellow
+    " hi @lsp.type.property   guifg=Green
+
+    " hi @lsp.typemod.function.classScope  guifg=Orange
+    " hi @lsp.typemod.variable.classScope  guifg=Orange
+    " hi @lsp.typemod.variable.fileScope   guifg=Orange
+    hi @lsp.typemod.variable.globalScope guifg=Red
+    ]])
+  end,
+})
 
 vim.cmd.colorscheme('rose-pine')
