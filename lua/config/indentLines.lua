b0cb76e15c07153f08b435b9609f9fd06c6b6d1b
Mina Sameh <mina.s.wadie@gmail.com>
2023-10-26 09:57:22 +0300

    updated indentlines config

--- lua/config/indentLines.lua
+++ lua/config/indentLines.lua
@@ -1,16 +1,35 @@
 -- TODO: look into https://github.com/lukas-reineke/indent-blankline.nvim/issues/672
 local function M()
-  vim.opt.list = true
-  vim.opt.listchars:append('space:⋅')
+  -- vim.opt.list = true
+  -- vim.opt.listchars:append('space:⋅')
   -- vim.opt.listchars:append('eol:↴')
-  vim.g.indentLine_char_list = { '|', '¦', '┆', '┊' }
-  vim.g.indent_blankline_context_char_list = { '┃', '║', '╬', '█' }
-  vim.g.indentLine_char_list = { '┃', '║', '╬', '█' }
-  vim.g.indent_blankline_use_treesitter = true
-  vim.g.indent_blankline_show_first_indent_level = false
+
+  --- Kept for chars
+  -- vim.g.indentLine_char_list = { '|', '¦', '┆', '┊' }
+  -- vim.g.indent_blankline_context_char_list = { '┃', '║', '╬', '█' }
+  -- vim.g.indentLine_char_list = { '┃', '║', '╬', '█' }
+  -- vim.g.indent_blankline_use_treesitter = true
+  -- vim.g.indent_blankline_show_first_indent_level = false
 
   return require('ibl').setup({
-    scope = { enabled = true },
+    scope = {
+      enabled = true,
+      char = '│',
+      show_start = true,
+      show_end = true,
+      show_exact_scope = true,
+    },
+    indent = {
+      -- char = '│',
+      char = { '¦', '┆', '┊' },
+      -- tab_char = "│",
+      smart_indent_cap = true,
+    },
+
+    whitespace = {
+      highlight = { 'Whitespace', 'NonText', 'Function', 'Label' },
+    },
+
     exclude = {
       filetypes = {
         'help',
@@ -19,6 +38,9 @@ local function M()
         'TelescopeResults',
         'checkhealth',
         'packer',
+        'log',
+        'todoist',
+        'neo-tree',
         'lspinfo',
         'toggleterm',
         'lsp-installer',
@@ -31,31 +53,6 @@ local function M()
         'help',
       },
     },
-    indent = {
-      char = '│',
-    },
-    -- indent = {
-    --   char = ' ',
-    -- },
-
-    ---- WARN: DEPRECATED:
-    -- use_treesitter = true,
-    -- buftype_exclude = { 'terminal', 'prompt', 'nofile', 'help' },
-    -- filetype_exclude = {
-    --   'TelescopePrompt',
-    --   'TelescopeResults',
-    --   'checkhealth',
-    --   'packer',
-    --   'lspinfo',
-    --   'toggleterm',
-    --   'lsp-installer',
-    --   'terminal',
-    -- },
-    -- show_end_of_line = true,
-    -- show_current_context_start = true,
-    -- leading_space = true,
-    -- show_first_indent_level = false,
-    -- show_current_context = true,
   })
 end
 
