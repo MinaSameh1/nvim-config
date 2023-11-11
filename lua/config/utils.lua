441811a2d33e44f638a73c86bed1a4f6739cbc9a
MinaSameh1 <minasameh1@gmail.com>
2023-11-11 08:25:16 +0200

    update

--- lua/config/utils.lua
+++ lua/config/utils.lua
@@ -83,15 +83,16 @@ utils.LSP_Name = {
   -- Lsp server name .
   function()
     local msg = 'No Active Lsp'
-    local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
-    local clients = vim.lsp.get_active_clients()
+    local buf_ft = vim.api.nvim_get_option_value('filetype', { buf = 0 })
+    local clients = vim.lsp.get_clients()
     if next(clients) == nil then
       return msg
     end
     for _, client in ipairs(clients) do
       local filetypes = client.config.filetypes
       if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-        return client.name
+        -- pick the first 10 chars from the name
+        return client.name:sub(1, 10)
       end
     end
     return msg
