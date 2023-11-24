e4ceb7158247e8cf00412a6c5c2a6a69be1a6c6e
MinaSameh1 <minasameh1@gmail.com>
2023-11-24 09:38:16 +0200

    fixed toggle inlay hints

--- lua/config/lsp/default_opts.lua
+++ lua/config/lsp/default_opts.lua
@@ -196,7 +196,11 @@ M.on_attach = function(client, bufnr)
   -- end
   if vim.lsp.inlay_hint then
     vim.keymap.set('n', '<leader>th', function()
-      vim.lsp.inlay_hint.enable(bufnr, false)
+      if vim.lsp.inlay_hint.is_enabled(bufnr) then
+        vim.lsp.inlay_hint.enable(bufnr, false)
+      else
+        vim.lsp.inlay_hint.enable(bufnr, true)
+      end
     end, { expr = true, noremap = true, desc = '[T]oggle inlay [h]ints' })
     vim.lsp.inlay_hint.enable(bufnr, true)
   end
