881571e800d3054e56f0ccfbec9711ef3b1e5f38
MinaSameh1 <minasameh1@gmail.com>
2023-12-22 04:31:20 +0200

    make it show recordings

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -13,6 +13,12 @@ return {
   'folke/noice.nvim',
   event = 'VeryLazy',
   opts = {
+    routes = {
+      {
+        view = 'notify',
+        filter = { event = 'msg_showmode' },
+      },
+    },
     background_colour = '#1e222a',
     lsp = {
       -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
@@ -32,10 +38,16 @@ return {
   },
   dependencies = {
     -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-    'MunifTanjim/nui.nvim',
+    {
+      'MunifTanjim/nui.nvim',
+      event = 'VeryLazy',
+    },
     -- OPTIONAL:
     --   `nvim-notify` is only needed, if you want to use the notification view.
     --   If not available, we use `mini` as the fallback
-    'rcarriga/nvim-notify',
+    {
+      'rcarriga/nvim-notify',
+      event = 'VeryLazy',
+    },
   },
 }
