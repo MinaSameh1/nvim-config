93b7989a2833c4b3cf64c7d0aff33516c164a512
MinaSameh1 <minasameh1@gmail.com>
2023-12-29 17:43:05 +0200

    added java snips

---
+++ lua/snippets/java.lua
@@ -0,0 +1,28 @@
+local s = require('snippets.helpers')
+
+return {
+  s.s(
+    {
+      trig = 'sout',
+      dsrc = 'System.out.println'
+    },
+    s.t('System.out.println("'),
+    s.i(1),
+    s.t('");')
+  ),
+  s.s(
+    { trig = 'time', dscr = 'Time code execution' },
+    s.fmta(
+      [[
+long startTime = System.currentTimeMillis();
+<>
+long endTime = System.currentTimeMillis();
+System.out.println("Execution time of <>: " + (endTime - startTime) + "ms");
+      ]],
+      {
+        s.d(1, s.get_visual),
+        s.i(2, 'name'),
+      }
+    )
+  ),
+}
