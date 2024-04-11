13270421f7f1623c05ffa59dabcad8c4c92f656b
Mina Sameh <mina.s.wadie@gmail.com>
2024-04-11 18:03:57 +0200

    add php snippets

---
+++ lua/snippets/php.lua
@@ -0,0 +1,11 @@
+local ls = require('snippets.helpers')
+
+return {
+  ls.s(
+    { trig = 'echo', dscr = 'Echo' },
+    ls.fmt([[echo "{}";]], {
+      ls.i(1),
+    }),
+    { condition = ls.conds.line_begin }
+  )
+}
