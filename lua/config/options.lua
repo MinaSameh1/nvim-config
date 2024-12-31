70b31cb2a0d81132cbba4beaf0df6589e61e208d
MinaSameh1 <MinaSameh1@users.noreply.github.com>
2024-12-31 07:29:30 +0000

    Apply automatic changes

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,10 +1,10 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 
-local utils = require("config.utils")
+local utils = require('config.utils')
 
 vim.opt.expandtab = true -- use spaces when <Tab> is inserted
 vim.opt.shiftwidth = 2 -- number of spaces to use for (auto)indent step
 vim.opt.tabstop = 2 -- number of spaces that <Tab> in file uses
 vim.opt.softtabstop = 2 -- number of spaces that <Tab> uses while editing
 
-vim.opt.undodir = utils.config_location .. "/undodir" -- where to store undo files
+vim.opt.undodir = utils.config_location .. '/undodir' -- where to store undo files
