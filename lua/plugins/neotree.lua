4cc397865ac7c1f0b06140725510ffd409071cac
MinaSameh1 <minasameh1@gmail.com>
2025-01-10 23:33:34 +0200

    add line numbers to neotree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,5 +1,15 @@
 return {
   'nvim-neo-tree/neo-tree.nvim',
+  opts = {
+    event_handlers = {
+      {
+        event = 'neo_tree_buffer_enter',
+        handler = function()
+          vim.opt.relativenumber = true
+        end,
+      },
+    },
+  },
   keys = {
 
     {
