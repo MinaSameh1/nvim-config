7541ecd317631c7280f762b70912cc3d6541bd71
Mina Sameh <mina.sameh@soar.inc>
2025-02-25 12:12:07 +0200

    add toggle for copilot

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -1,6 +1,9 @@
 -- change github copilot suggestion color
 -- vim.cmd('hi CopilotSuggestion guifg=#555555 ctermfg=8')
 
+local Snacks = require('snacks')
+local copilot_exists = pcall(require, 'copilot')
+
 -- Map github copilot suggestion key
 vim.api.nvim_set_keymap('i', '<C-\\>', 'copilot#Accept("")', {
   noremap = false,
@@ -8,3 +11,23 @@ vim.api.nvim_set_keymap('i', '<C-\\>', 'copilot#Accept("")', {
   silent = true,
   desc = 'Accept Copilot Suggestion',
 })
+
+if copilot_exists then
+  Snacks.toggle({
+    name = 'Copilot Completion',
+    color = {
+      enabled = 'azure',
+      disabled = 'orange',
+    },
+    get = function()
+      return not require('copilot.client').is_disabled()
+    end,
+    set = function(state)
+      if state then
+        require('copilot.command').enable()
+      else
+        require('copilot.command').disable()
+      end
+    end,
+  }):map('<leader>at')
+end
